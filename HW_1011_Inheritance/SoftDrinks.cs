809f40a313dc6995206f1992729dd5c5d9613fe3
KathleenRepko <kathleen.repko@gmail.com>
2017-10-11 20:50:44 -0400

    Created base and derived classes.

---
+++ HW_1011_Inheritance/SoftDrinks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1011_Inheritance
+{
+    class SoftDrinks : Beverages
+    {
+        private bool hasCaffeine;
+        private bool isDiet;
+
+        public bool HasCaffeine
+        {
+            get { return this.hasCaffeine; }
+            set { this.hasCaffeine = hasCaffeine; }
+        }
+
+
+        SoftDrinks(int volume, bool isFizzy, string color, bool isClear, bool hasCaffeine, bool isDiet)
+            
+        {
+            this.Volume = volume;
+            this.isFizzy = isFizzy;
+            this.Color = color;
+            this.isClear = isClear;
+            this.hasCaffeine = hasCaffeine;
+            this.isDiet = isDiet;
+        }
+}
